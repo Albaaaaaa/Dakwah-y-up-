c6c97b7c724f27cf1b892545f68de0525b2e9e3e
Albaaaaaa <ulilalbaa05@gmail.com>
2024-04-27 11:04:32 +0800

    Tampilan Splash Screen

---
+++ android/app/src/main/kotlin/com/example/dakwah_y_up/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dakwah_y_up
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/bahan.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+Color latarbelakang = const Color(0xFF040C23);
+Color text = const Color(0xFFFFFFFF);
+Color grey = const Color(0xFF84A59F);
+Color stroke = const Color(0xFF1C6758);
+
+
+
