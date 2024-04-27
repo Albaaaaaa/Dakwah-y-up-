2913e5a2fe71662db66e722476cbf99645f02274
Albaaaaaa <ulilalbaa05@gmail.com>
2024-04-27 14:43:06 +0800

    Footer halaman home

--- lib/bahan.dart
+++ lib/bahan.dart
@@ -5,6 +5,7 @@ Color latarbelakang = const Color(0xFF040C23);
 Color text = const Color(0xFFFFFFFF);
 Color grey = const Color(0xFF84A59F);
 Color stroke = const Color(0xFF1C6758);
+Color footer = const Color(0xFF121931);
 
 
 
--- lib/layar_home/home.dart
+++ lib/layar_home/home.dart
@@ -10,35 +10,62 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: latarbelakang,
-      appBar: _appBar(),
+      appBar: _buildAppBar(),
+      bottomNavigationBar: _buildBottomNavigationBar(),
       body: SafeArea(child: Text('home screen')),
     );
   }
 
-  AppBar _appBar() => AppBar(
-        backgroundColor: latarbelakang,
-        automaticallyImplyLeading: false,
-        elevation: 0,
-        title: Row(
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset('assets/svgs/navbar_menu_home.svg'),
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: latarbelakang,
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      title: Row(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('assets/svgs/navbar_menu_home.svg'),
+          ),
+          Text(
+            "Dakwah'y up",
+            style: GoogleFonts.poppins(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
             ),
-            Text(
-              "Dakwah'y up",
-              style: GoogleFonts.poppins(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            const Spacer(),
-            IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset('assets/svgs/pencarian_menu_home.svg'),
-            ),
-          ],
-        ),
-      );
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('assets/svgs/pencarian_menu_home.svg'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar() {
+return BottomNavigationBar(
+    type: BottomNavigationBarType.fixed,
+    backgroundColor: footer,
+    showSelectedLabels: false,
+    showUnselectedLabels: false,
+    items: [
+      _footerBarItem(icon: "assets/svgs/footer_quran.svg", label: "quran"),
+      _footerBarItem(icon: "assets/svgs/footer_lampu.svg", label: "Lampu"),
+      _footerBarItem(icon: "assets/svgs/footer_orang_duduk.svg", label: "Icon Orang Duduk"),
+      _footerBarItem(icon: "assets/svgs/footer_tangan_berdoa.svg", label: "Icon Tangan Berdoa"),
+      _footerBarItem(icon: "assets/svgs/footer_simpan.svg", label: "Icon Simpan"),
+    ],
+  );
+}
+
+BottomNavigationBarItem _footerBarItem({required String icon, required String label}) {
+  return BottomNavigationBarItem(
+    icon: SvgPicture.asset(icon),
+    label: label,
+  );
+}
+
 }
