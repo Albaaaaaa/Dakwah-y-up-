537bef3a5ff955e1cc45df172832dc4f00aa0fa3
Albaaaaaa <ulilalbaa05@gmail.com>
2024-04-27 12:12:44 +0800

    NavBar inc menu & pencarian

--- lib/layar_home/home.dart
+++ lib/layar_home/home.dart
@@ -1,4 +1,7 @@
+import 'package:dakwah_y_up/bahan.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +9,36 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: latarbelakang,
+      appBar: _appBar(),
       body: SafeArea(child: Text('home screen')),
     );
   }
+
+  AppBar _appBar() => AppBar(
+        backgroundColor: latarbelakang,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        title: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset('assets/svgs/navbar_menu_home.svg'),
+            ),
+            Text(
+              "Dakwah'y up",
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset('assets/svgs/pencarian_menu_home.svg'),
+            ),
+          ],
+        ),
+      );
 }
